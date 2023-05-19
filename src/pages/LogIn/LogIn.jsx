50ca6b3fc2c032c1a2e679e44f9c93f93dd46354
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-19 19:14:41 +0600

    design login page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 
 const LogIn = () => {
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+            <div className=" w-1/2">
+
+                <img src='' alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h1 className="text-4xl font-bold">Login now!</h1>
+                    <form >
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                           
+                        </div>
+                        <div className="form-control mt-6">
+
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p>Don't Have any Account <Link className=' text-blue-400 link' to="/register">Register A Account</Link></p>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
