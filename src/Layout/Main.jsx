eabafc9a3c85725284f5220c570b24a9203a0219
Sheikhmsip <sheikhmsip@gmail.com>
2023-05-20 02:41:20 +0600

    added Footer section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import NavBar from '../pages/SharePage/Header/NavBar';
 import { Toaster } from 'react-hot-toast';
+import Footer from '../pages/SharePage/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <Toaster></Toaster>
         </div>
     );
---
+++ src/pages/SharePage/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { CiFacebook } from "react-icons/ci";
+import { FaInstagramSquare} from "react-icons/fa";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white">
+      <div className="container mx-auto flex flex-wrap items-center justify-between p-4">
+        <div className="flex items-center">
+          <img src="https://i.ibb.co/SXgGjSD/attachment.png" alt="Website Logo" className=" mr-2" />
+          <span className="text-lg font-bold">All Right Received TOY STARS</span>
+        </div>
+        <div className='flex items-center gap-5'>
+            <h2 className='text-xl font-bold'>
+                Social Link 
+            </h2>
+            <AiOutlineDoubleRight></AiOutlineDoubleRight>
+            <AiOutlineDoubleRight></AiOutlineDoubleRight>
+        </div>
+        <div className="flex items-center space-x-4">
+
+          <Link  className="text-gray-300 hover:text-white">
+          <FaInstagramSquare className='w-8 h-10  text-[#f36709]'></FaInstagramSquare>
+          </Link>
+          <Link to='https://www.facebook.com' className="text-gray-300 hover:text-white">
+            <CiFacebook className='w-10 h-10 text-blue-600'></CiFacebook>
+          </Link>
+        </div>
+        <div className="text-gray-300 mt-4 text-2xl">
+            <div className='py-3'>
+                <h3 className='text-2xl font-bold text-white'>Address</h3>
+            </div>
+          <div>
+          <p>123 Street, City</p>
+          <p>State, Country</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    );
+};
+
+export default Footer;
